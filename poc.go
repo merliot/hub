e522a52a4947946eb6440d3d543c58bb2705ad54
Scott Feldman <sfeldma@gmail.com>
2023-06-25 22:25:42 -0700

    revert back to basic hub

--- poc.go
+++ poc.go
@@ -1,15 +1,9 @@
 package poc
 
 import (
-	"embed"
-	"net/http"
-
 	"github.com/merliot/dean"
 	"github.com/merliot/dean-lib/hub"
 )
-
-//go:embed css js index.html
-var fs embed.FS
 
 type Poc struct {
 	*hub.Hub
@@ -20,8 +14,4 @@ func New(id, model, name string) dean.Thinger {
 	return &Poc{
 		Hub: hub.New(id, model, name).(*hub.Hub),
 	}
-}
-
-func (p *Poc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.ServeFS(fs, w, r)
 }
