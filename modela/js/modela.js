3181d4e723a9fa9257cd9df9b194ec6b730a5888
Scott Feldman <sfeldma@gmail.com>
2023-06-27 15:54:57 -0700

    work on modela

--- modela/js/modela.js
+++ modela/js/modela.js
@@ -2,74 +2,28 @@ var state
 var conn
 var online = false
 
-var opts = {
-	angle: -0.2, // The span of the gauge arc
-	lineWidth: 0.2, // The line thickness
-	radiusScale: 1, // Relative radius
-	pointer: {
-		length: 0.6, // // Relative to gauge radius
-		strokeWidth: 0.035, // The thickness
-		color: '#000000' // Fill color
-	},
-	limitMax: true,      // If false, max value increases automatically if value > maxValue
-	limitMin: false,     // If true, the min value of the gauge will be fixed
-	highDpiSupport: true,     // High resolution support
-	staticZones: [
-		{strokeStyle: "#30B32D", min:      0, max:  650000}, // Green
-		{strokeStyle: "#FFDD00", min: 650000, max:  700000}, // Yellow
-		{strokeStyle: "#F03E3E", min: 700000, max: 1000000}  // Red
-	],
-}
-var bh1750= document.getElementById('bh1750')
-var gauge = new Gauge(bh1750).setOptions(opts)
-
-gauge.maxValue = 1000000
-gauge.setMinValue(0)
-gauge.animationSpeed = 32
-gauge.set(0)
-
-function showTemp() {
-	let tempc = document.getElementById("tempc")
-	tempc.value = ""
-	tempc.value = "Temperature:     " + state.TempC + "(C)"
+var relays = []
+for (var i = 0; i < 4; i++) {
+	relays[i] = document.getElementById("relay" + i)
 }
 
-function showSystem() {
-	let system = document.getElementById("system")
-	system.value = ""
-	system.value += "CPU Frequency:   " + state.CPUFreq + "Mhz\r\n"
-	system.value += "MAC Address:     " + state.Mac + "\r\n"
-	system.value += "IP Address:      " + state.Ip
-}
+var buttons = document.getElementById("buttons")
 
-function showBH1750() {
-	gauge.set(state.Lux)
-}
-
-function showRelay() {
-	let relay = document.getElementById("relay")
-	if (650000 <= state.Lux && state.Lux <= 700000) {
-		relay.src = "images/relay-on.svg"
-	} else {
-		relay.src = "images/relay-off.svg"
+function showRelays() {
+	for (var i = 0; i < relays.length; i++) {
+		relays[i].disabled = !online
 	}
+	buttons.style.display = "block"
 }
 
 function show() {
 	overlay = document.getElementById("overlay")
 	overlay.style.display = online ? "none" : "block"
-	showSystem()
-	showTemp()
-	showBH1750()
-	showRelay()
+	showRelays()
 }
 
-function reset() {
-	state.Lux = 0
-	showBH1750()
-	showRelay()
-	conn.send(JSON.stringify({Path: "lux", Lux: 0}))
-	conn.send(JSON.stringify({Path: "reset"}))
+function sendClick(relay, num) {
+	conn.send(JSON.stringify({Path: "click", Relay: num, State: relay.checked}))
 }
 
 function run(ws) {
@@ -101,19 +55,11 @@ function run(ws) {
 		switch(msg.Path) {
 		case "state":
 			online = true
-			// fall-thru
-		case "update":
 			state = msg
 			show()
 			break
-		case "lux":
-			state.Lux = msg.Lux
-			showBH1750()
-			showRelay()
-			break
-		case "tempc":
-			state.TempC = msg.TempC
-			showTemp()
+		case "click":
+			relays[msg.Relay].checked = msg.State
 			break
 		}
 	}
--- modela/modela.go
+++ modela/modela.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/merliot/dean"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
 )
 
 //go:embed css js images index.html
@@ -13,6 +15,14 @@ var fs embed.FS
 type Modela struct {
 	dean.Thing
 	dean.ThingMsg
+	relays [4]*gpio.RelayDriver
+	States [4]bool
+}
+
+type MsgClick struct {
+	Path  string
+	Relay int
+	State bool
 }
 
 func New(id, model, name string) dean.Thinger {
@@ -31,18 +41,47 @@ func (m *Modela) getState(msg *dean.Msg) {
 	msg.Marshal(m).Reply()
 }
 
-func (m *Modela) update(msg *dean.Msg) {
-	msg.Unmarshal(m).Broadcast()
+func (m *Modela) click(msg *dean.Msg) {
+	var msgClick MsgClick
+	msg.Unmarshal(&msgClick)
+	m.States[msgClick.Relay] = msgClick.State
+	println(m.IsMetal(), msgClick.State)
+	if m.IsMetal() {
+		if msgClick.State {
+			m.relays[msgClick.Relay].On()
+		} else {
+			m.relays[msgClick.Relay].Off()
+		}
+	}
+	msg.Broadcast()
 }
 
 func (m *Modela) Subscribers() dean.Subscribers {
 	return dean.Subscribers{
 		"state":     m.saveState,
 		"get/state": m.getState,
-		"update":    m.update,
+		"click":     m.click,
 	}
 }
 
 func (m *Modela) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	m.ServeFS(fs, w, r)
+}
+
+func (m *Modela) Run(i *dean.Injector) {
+
+	adaptor := raspi.NewAdaptor()
+	adaptor.Connect()
+
+	m.relays[0] = gpio.NewRelayDriver(adaptor, "31") // GPIO 6
+	m.relays[1] = gpio.NewRelayDriver(adaptor, "33") // GPIO 13
+	m.relays[2] = gpio.NewRelayDriver(adaptor, "35") // GPIO 19
+	m.relays[3] = gpio.NewRelayDriver(adaptor, "37") // GPIO 26
+
+	for _, relay := range m.relays {
+		relay.Start()
+		relay.On()
+	}
+
+	select {}
 }
