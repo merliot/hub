1c5b8e6df2e7197a1628bd51912fc43276475500
Scott Feldman <sfeldma@gmail.com>
2023-06-28 22:59:11 -0700

    sense: switch to bh1750

--- models/sense/js/sense.js
+++ models/sense/js/sense.js
@@ -2,6 +2,32 @@ var state
 var conn
 var online = false
 
+var opts = {
+	angle: -0.2, // The span of the gauge arc
+	lineWidth: 0.2, // The line thickness
+	radiusScale: 1, // Relative radius
+	pointer: {
+		length: 0.6, // // Relative to gauge radius
+		strokeWidth: 0.035, // The thickness
+		color: '#000000' // Fill color
+	},
+	limitMax: true,      // If false, max value increases automatically if value > maxValue
+	limitMin: false,     // If true, the min value of the gauge will be fixed
+	highDpiSupport: true,     // High resolution support
+	staticZones: [
+		{strokeStyle: "#30B32D", min:      0, max: 1700}, // Green
+		{strokeStyle: "#FFDD00", min:   1700, max: 1780}, // Yellow
+		{strokeStyle: "#F03E3E", min:   1780, max: 2400}  // Red
+	],
+}
+var bh1750= document.getElementById('bh1750')
+var gauge = new Gauge(bh1750).setOptions(opts)
+
+gauge.maxValue = 2400
+gauge.setMinValue(0)
+gauge.animationSpeed = 32
+gauge.set(0)
+
 function showSystem() {
 	let system = document.getElementById("system")
 	system.value = ""
@@ -10,10 +36,15 @@ function showSystem() {
 	system.value += "Name:    " + state.Identity.Name
 }
 
+function showLux() {
+	gauge.set(state.Lux)
+}
+
 function show() {
 	overlay = document.getElementById("overlay")
 	overlay.style.display = online ? "none" : "block"
 	showSystem()
+	showLux()
 }
 
 function run(ws) {
@@ -47,6 +78,10 @@ function run(ws) {
 			online = true
 			state = msg
 			show()
+			break
+		case "update":
+			state.Lux = msg.Lux
+			showLux()
 			break
 		}
 	}
--- models/sense/sense.go
+++ models/sense/sense.go
@@ -3,9 +3,10 @@ package sense
 import (
 	"embed"
 	"net/http"
+	"time"
 
 	"github.com/merliot/dean"
-	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/drivers/i2c"
 	"gobot.io/x/gobot/platforms/raspi"
 )
 
@@ -16,8 +17,8 @@ type Sense struct {
 	dean.Thing
 	dean.ThingMsg
 	Identity Identity
-	sense [4]*gpio.RelayDriver
-	States [4]bool
+	Lux int
+	bh1750 *i2c.BH1750Driver
 }
 
 type Identity struct {
@@ -26,8 +27,13 @@ type Identity struct {
 	Name  string
 }
 
+type Update struct {
+	Path string
+	Lux int
+}
+
 func New(id, model, name string) dean.Thinger {
-	println("NEW RELAYS")
+	println("NEW SENSE")
 	return &Sense{
 		Thing: dean.NewThing(id, model, name),
 		Identity: Identity{id, model, name},
@@ -43,10 +49,15 @@ func (s *Sense) getState(msg *dean.Msg) {
 	msg.Marshal(s).Reply()
 }
 
+func (s *Sense) update(msg *dean.Msg) {
+	msg.Unmarshal(s).Broadcast()
+}
+
 func (s *Sense) Subscribers() dean.Subscribers {
 	return dean.Subscribers{
 		"state":     s.saveState,
 		"get/state": s.getState,
+		"update":    s.update,
 	}
 }
 
@@ -55,9 +66,30 @@ func (s *Sense) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Sense) Run(i *dean.Injector) {
+	var msg dean.Msg
+	var update = Update{Path: "update"}
 
 	adaptor := raspi.NewAdaptor()
 	adaptor.Connect()
+
+	s.bh1750 = i2c.NewBH1750Driver(adaptor)
+	err := s.bh1750.Start()
+	if err != nil {
+		println(err.Error())
+	}
+
+	for {
+		lux, err := s.bh1750.Lux()
+		if err != nil {
+			println(err.Error())
+		}
+		if lux != s.Lux {
+			s.Lux = lux
+			update.Lux  = lux
+			i.Inject(msg.Marshal(update))
+		}
+		time.Sleep(time.Second)
+	}
 
 	select {}
 }
