52e6b31b1b2b4d00f18889bebf411146c0b0f925
Scott Feldman <sfeldma@gmail.com>
2023-09-08 23:45:22 -0700

    hard-code relays list to 4 static relays

--- models/relays/js/deploy.js
+++ models/relays/js/deploy.js
@@ -1,93 +1,63 @@
-const sharedItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8"];
+const deployTargetGpios = {
+    "x86-64": [],
+    "rpi": ["gpio20", "gpio23", "gpio25", "gpio30"],
+    "nano-rp2040": ["gpio0", "gpio3", "gpio25", "gpio3"],
+};
 
-function populateDropdown(dropdown, exclude = []) {
-	dropdown.innerHTML = '<option value="">Select Item</option>'; // Default option
-	sharedItems.forEach(gpio => {
-		if (!exclude.includes(gpio)) {
-			let option = document.createElement('option');
-			option.value = gpio;
-			option.textContent = gpio;
-			dropdown.appendChild(option);
-		}
-	});
+let target = document.getElementById('deploy-target')
+let gpios = deployTargetGpios[target.value];
+
+// Function to refresh all select options based on current selections
+function refreshGpioOptions() {
+    const selects = document.querySelectorAll('.gpio');
+    const usedGpios = [];
+
+    // Find all currently selected GPIOs
+    selects.forEach(select => {
+        const value = select.value;
+        if (value) {
+            usedGpios.push(value);
+        }
+    });
+
+    // Refresh options in all select dropdowns
+    selects.forEach(select => {
+        const currentValue = select.value;
+        let optionsHtml = '<option value="">Select GPIO</option>';
+
+        gpios.forEach(gpio => {
+            // If gpio is not used or it's the current value of this select, add it as an option
+            if (!usedGpios.includes(gpio) || gpio === currentValue) {
+                optionsHtml += `<option value="${gpio}">${gpio}</option>`;
+            }
+        });
+
+        select.innerHTML = optionsHtml;
+        select.value = currentValue;  // Restore the previously selected value
+    });
 }
 
-function updateAllDropdowns() {
-	const dropdowns = document.getElementsByClassName('gpio');
-	const selectedValues = Array.from(dropdowns).map(d => d.value).filter(v => v !== ""); // Exclude empty selections
-
-	for (const dropdown of dropdowns) {
-		const currentValue = dropdown.value;
-		const exclude = [...selectedValues];
-		if(currentValue) {
-			exclude.splice(exclude.indexOf(currentValue), 1); // Keep the current value in its dropdown
-		}
-		populateDropdown(dropdown, exclude);
-		dropdown.value = currentValue; // Restore the selected value after repopulating
-	}
+function clearGpioOptions() {
+    const selects = document.querySelectorAll('.gpio');
+    selects.forEach(select => {
+        select.value = ""
+    });
 }
 
-function addRelay() {
-	const relaysList = document.getElementById('relaysList');
-	const existingNames = relaysList.getElementsByClassName('relayItem');
+// Attach an event listener to the deploy-target dropdown to adjust available GPIOs
+target.addEventListener('change', function() {
+    const selectedTarget = this.value;
+    if (deployTargetGpios[selectedTarget]) {
+        gpios = deployTargetGpios[selectedTarget];
+	clearGpioOptions();
+        refreshGpioOptions();
+    }
+});
 
-	if(existingNames.length < sharedItems.length) {
-		const newItem = document.createElement('div');
-		newItem.classList.add('relayItem', 'divFlexRow');
+// Attach event listeners to the gpio dropdowns to ensure no duplicate selections
+document.querySelectorAll('.gpio').forEach(select => {
+    select.addEventListener('change', refreshGpioOptions);
+});
 
-		const label = document.createElement('label');
-		label.classList.add('relayLabel');
-		label.textContent = 'Relay #' + (existingNames.length + 1);
-
-		const input = document.createElement('input');
-		input.type = 'text';
-		input.placeholder = 'Name';
-		input.name = 'relay_' + (existingNames.length + 1);
-
-		const dropdown = document.createElement('select');
-		dropdown.classList.add('gpio');
-		dropdown.onchange = updateAllDropdowns;
-		dropdown.name = 'gpio_' + (existingNames.length + 1);
-
-		const trashIcon = document.createElement('span');
-		trashIcon.classList.add('trashIcon');
-		trashIcon.innerHTML = '&#x1F5D1;';
-		trashIcon.onclick = function() {
-			deleteRelay(trashIcon);
-		};
-
-		newItem.appendChild(label);
-		newItem.appendChild(input);
-		newItem.appendChild(dropdown);
-		newItem.appendChild(trashIcon);
-
-		relaysList.appendChild(newItem);
-		updateAllDropdowns();
-	} else {
-		alert('Maximum relays reached.');
-	}
-}
-
-function deleteRelay(deleteIconElement) {
-	const relaysList = document.getElementById('relaysList');
-	const relayItem = deleteIconElement.closest('.relayItem');
-	relaysList.removeChild(relayItem);
-
-	const existingNames = relaysList.getElementsByClassName('relayItem');
-	if (existingNames.length === 0) {
-		addRelay();
-	} else {
-		// Renumber the relays
-		for (let i = 0; i < existingNames.length; i++) {
-			existingNames[i].querySelector('.relayLabel').textContent = 'Relay #' + (i + 1);
-		}
-		updateAllDropdowns();
-	}
-}
-
-// On page load, populate the initial dropdown
-window.onload = function() {
-	const initialDropdown = document.querySelector('.gpio');
-	populateDropdown(initialDropdown);
-}
-
+// Initial population of the GPIO options
+refreshGpioOptions();
