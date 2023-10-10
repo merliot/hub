9a4bc770fa81551878b49107e1e3d4cf804292a6
Scott Feldman <sfeldma@gmail.com>
2023-10-10 13:05:06 -0700

    testing env var with single quote

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -16,8 +16,6 @@ func main() {
 
 	h.ParseWifiAuth()
 
-	println("TEST=", os.Getenv("TEST"))
-
 	gitRemote := os.Getenv("GIT_REMOTE")
 	gitKey := os.Getenv("GIT_KEY")
 	gitAuthor := os.Getenv("GIT_AUTHOR")
