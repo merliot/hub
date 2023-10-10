e7a3ff5cb2b9b14c2eebd58a3fc917ff0923e1ed
Scott Feldman <sfeldma@gmail.com>
2023-10-10 13:18:59 -0700

    working on backup hub mode

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -21,8 +21,8 @@ func main() {
 	gitAuthor := os.Getenv("GIT_AUTHOR")
 	h.SetGit(gitRemote, gitKey, gitAuthor)
 
-	h.BackupHubURL := os.Getenv("BACKUP_HUB_URL")
-	h.BackupHub := os.Getenv("BACKUP_HUB")
+	h.BackupHubURL = os.Getenv("BACKUP_HUB_URL")
+	_, h.BackupHub = os.LookupEnv("BACKUP_HUB")
 
 	server := dean.NewServer(h)
 	h.SetServer(server)
