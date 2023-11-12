efb74656ec69180c83f6f1b85c5f6e9067bd63c6
Scott Feldman <sfeldma@gmail.com>
2023-11-11 21:23:10 -0800

    hp2430n: adjust html spacing

--- models/hp2430n/js/hp2430n.js
+++ models/hp2430n/js/hp2430n.js
@@ -34,14 +34,14 @@ class Hp2430n extends WebSocketController {
 	showSystem() {
 		var ta = document.getElementById("system")
 		ta.value = ""
-		ta.value += "Maximum Voltage Supported (V):  " + this.state.System.MaxVolts + "\r\n"
-		ta.value += "Rated Charging Current (A):     " + this.state.System.ChargeAmps + "\r\n"
-		ta.value += "Rated Discharging Current (A):  " + this.state.System.DischargeAmps + "\r\n"
-		ta.value += "Product Type:                   " + this.state.System.ProductType + "\r\n"
-		ta.value += "Model:                          " + this.state.System.Model + "\r\n"
-		ta.value += "Software Version:               " + this.state.System.SWVersion + "\r\n"
-		ta.value += "Hardware Version:               " + this.state.System.HWVersion + "\r\n"
-		ta.value += "Serial:                         " + this.state.System.Serial
+		ta.value += "Max Voltage Supported (V):   " + this.state.System.MaxVolts + "\r\n"
+		ta.value += "Rated Charge Current (A):    " + this.state.System.ChargeAmps + "\r\n"
+		ta.value += "Rated Discharge Current (A): " + this.state.System.DischargeAmps + "\r\n"
+		ta.value += "Product Type:                " + this.state.System.ProductType + "\r\n"
+		ta.value += "Model:                       " + this.state.System.Model + "\r\n"
+		ta.value += "Software Version:            " + this.state.System.SWVersion + "\r\n"
+		ta.value += "Hardware Version:            " + this.state.System.HWVersion + "\r\n"
+		ta.value += "Serial:                      " + this.state.System.Serial
 	}
 
 	showAlarms() {
@@ -55,8 +55,8 @@ class Hp2430n extends WebSocketController {
 	showController() {
 		var ta = document.getElementById("controller")
 		ta.value = ""
-		ta.value += "* Temp (C):                     " + this.state.Controller.Temp + "\r\n"
-		ta.value += "* Alarms:                       "
+		ta.value += "* Temp (C):                  " + this.state.Controller.Temp + "\r\n"
+		ta.value += "* Alarms:                    "
 		if (this.state.Controller.Alarms === null) {
 			ta.rows = 2
 			ta.value += "<none>"
@@ -69,54 +69,54 @@ class Hp2430n extends WebSocketController {
 	showBattery() {
 		var ta = document.getElementById("battery")
 		ta.value = ""
-		ta.value += "* Capacity SOC:                 " + this.state.Battery.SOC + "\r\n"
-		ta.value += "* Voltage (V):                  " + this.state.Battery.Volts + "\r\n"
-		ta.value += "* Current (A):                  " + this.state.Battery.Amps + "\r\n"
-		ta.value += "* Temp (C):                     " + this.state.Battery.Temp + "\r\n"
-		ta.value += "* Charging State:               " + this.state.Battery.ChargeState
+		ta.value += "* Capacity SOC:              " + this.state.Battery.SOC + "\r\n"
+		ta.value += "* Voltage (V):               " + this.state.Battery.Volts + "\r\n"
+		ta.value += "* Current (A):               " + this.state.Battery.Amps + "\r\n"
+		ta.value += "* Temp (C):                  " + this.state.Battery.Temp + "\r\n"
+		ta.value += "* Charging State:            " + this.state.Battery.ChargeState
 	}
 
 	showLoadInfo() {
 		var ta = document.getElementById("load")
 		ta.value = ""
-		ta.value += "* Voltage (V):                  " + this.state.LoadInfo.Volts + "\r\n"
-		ta.value += "* Current (A):                  " + this.state.LoadInfo.Amps + "\r\n"
-		ta.value += "* Status:                       " + this.state.LoadInfo.Status + "\r\n"
-		ta.value += "* Brightness:                   " + this.state.LoadInfo.Brightness
+		ta.value += "* Voltage (V):               " + this.state.LoadInfo.Volts + "\r\n"
+		ta.value += "* Current (A):               " + this.state.LoadInfo.Amps + "\r\n"
+		ta.value += "* Status:                    " + this.state.LoadInfo.Status + "\r\n"
+		ta.value += "* Brightness:                " + this.state.LoadInfo.Brightness
 	}
 
 	showSolar() {
 		var ta = document.getElementById("solar")
 		ta.value = ""
-		ta.value += "* Voltage (V):                  " + this.state.Solar.Volts + "\r\n"
-		ta.value += "* Current (A):                  " + this.state.Solar.Amps
+		ta.value += "* Voltage (V):               " + this.state.Solar.Volts + "\r\n"
+		ta.value += "* Current (A):               " + this.state.Solar.Amps
 	}
 
 	showDaily() {
 		var ta = document.getElementById("daily")
 		ta.value = ""
-		ta.value += "Battery Min Voltage (V):        " + this.state.Daily.BattMinVolts + "\r\n"
-		ta.value += "Battery Max Voltage (V):        " + this.state.Daily.BattMaxVolts + "\r\n"
-		ta.value += "Charging Max Current (A):       " + this.state.Daily.ChargeMaxAmps + "\r\n"
-		ta.value += "Discharging Max Current (A):    " + this.state.Daily.DischargeMaxAmps + "\r\n"
-		ta.value += "Charging Max Power (W):         " + this.state.Daily.ChargeMaxWatts + "\r\n"
-		ta.value += "Disharging Max Power (W):       " + this.state.Daily.DischargeMaxWatts + "\r\n"
-		ta.value += "Charge Current (Ah):            " + this.state.Daily.ChargeAmpHrs + "\r\n"
-		ta.value += "Discharge Current (Ah):         " + this.state.Daily.DischargeAmpHrs + "\r\n"
-		ta.value += "Power Generated (W):            " + this.state.Daily.GenPowerWatts + "\r\n"
-		ta.value += "Power Consumed (W):             " + this.state.Daily.ConPowerWatts
+		ta.value += "Battery Min Voltage (V):     " + this.state.Daily.BattMinVolts + "\r\n"
+		ta.value += "Battery Max Voltage (V):     " + this.state.Daily.BattMaxVolts + "\r\n"
+		ta.value += "Charging Max Current (A):    " + this.state.Daily.ChargeMaxAmps + "\r\n"
+		ta.value += "Discharging Max Current (A): " + this.state.Daily.DischargeMaxAmps + "\r\n"
+		ta.value += "Charging Max Power (W):      " + this.state.Daily.ChargeMaxWatts + "\r\n"
+		ta.value += "Disharging Max Power (W):    " + this.state.Daily.DischargeMaxWatts + "\r\n"
+		ta.value += "Charge Current (Ah):         " + this.state.Daily.ChargeAmpHrs + "\r\n"
+		ta.value += "Discharge Current (Ah):      " + this.state.Daily.DischargeAmpHrs + "\r\n"
+		ta.value += "Power Generated (W):         " + this.state.Daily.GenPowerWatts + "\r\n"
+		ta.value += "Power Consumed (W):          " + this.state.Daily.ConPowerWatts
 	}
 
 	showHistorical() {
 		var ta = document.getElementById("historical")
 		ta.value = ""
-		ta.value += "Total Operating Days:           " + this.state.Historical.OpDays + "\r\n"
-		ta.value += "Total Battery Over Discharges:  " + this.state.Historical.OverDischarges + "\r\n"
-		ta.value += "Total Battery Full Charges:     " + this.state.Historical.FullCharges + "\r\n"
-		ta.value += "Total Charge Current (Ah):      " + this.state.Historical.ChargeAmpHrs + "\r\n"
-		ta.value += "Total Discharge Current (Ah):   " + this.state.Historical.DischargeAmpHrs + "\r\n"
-		ta.value += "Total Power Generated (W):      " + this.state.Historical.GenPowerWatts + "\r\n"
-		ta.value += "Total Power Consumed (W):       " + this.state.Historical.ConPowerWatts
+		ta.value += "Total Operating Days:        " + this.state.Historical.OpDays + "\r\n"
+		ta.value += "Total Batt Over Discharges:  " + this.state.Historical.OverDischarges + "\r\n"
+		ta.value += "Total Battery Full Charges:  " + this.state.Historical.FullCharges + "\r\n"
+		ta.value += "Total Charge Current (Ah):   " + this.state.Historical.ChargeAmpHrs + "\r\n"
+		ta.value += "Total Discharge Cur (Ah):    " + this.state.Historical.DischargeAmpHrs + "\r\n"
+		ta.value += "Total Power Generated (W):   " + this.state.Historical.GenPowerWatts + "\r\n"
+		ta.value += "Total Power Consumed (W):    " + this.state.Historical.ConPowerWatts
 	}
 
 	handle(msg) {
