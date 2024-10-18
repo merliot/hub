655f226a06e4ff81a547b3115369b93f62e9e3b6
Scott Feldman <sfeldma@gmail.com>
2024-10-17 21:50:42 -0700

    merge left overs

---
+++ cmd/gen-models/main.go
@@ -0,0 +1,84 @@
+//go:generate go run ./
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"text/template"
+)
+
+type model struct {
+	Package string
+	Source  string
+	Maker   string
+}
+
+type models map[string]model
+
+const modelsTemplate = `// This file auto-generated from ./cmd/gen-models using models.json as input
+
+package models
+
+import (
+	"github.com/merliot/hub"
+{{- range . }}
+	"{{ .Package }}"
+{{- end }}
+)
+
+var AllModels = hub.ModelMap{
+{{- range $key, $value := . }}
+	"{{$key}}": {{title $key}},
+{{- end }}
+}
+
+{{- range $key, $value := . }}
+var {{title $key}} = hub.Model{
+	Package: "{{$value.Package}}",
+	Source: "{{$value.Source}}",
+	Maker: {{$value.Maker}},
+}
+
+{{- end }}
+`
+
+func main() {
+
+	var models models
+
+	data, err := ioutil.ReadFile("../../models.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(data, &models)
+	if err != nil {
+		panic(err)
+	}
+
+	outFile, err := os.Create("../../models/models.go")
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+
+	// Use template to write the models.go file
+	tmpl, err := template.New("models").Funcs(template.FuncMap{
+		"title": func(s string) string {
+			return strings.Title(s)
+		},
+	}).Parse(modelsTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	// Execute the template with the models data
+	if err := tmpl.Execute(outFile, models); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Created Models from models.json")
+}
