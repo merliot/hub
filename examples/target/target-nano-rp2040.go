9a551bd51d37b0b4bd20239fa091dc544756b53b
Scott Feldman <sfeldma@gmail.com>
2024-10-18 23:41:21 -0700

    remove some mis-copied dirs

--- examples/target/target-nano-rp2040.go
+++
@@ -1,8 +0,0 @@
-//go:build nano_rp2040
-
-package target
-
-func Pin(pin string) (GpioPin, bool) {
-	gpio, ok := AllTargets["nano-rp2040"].GpioPins[pin]
-	return gpio, ok
-}
--- examples/target/target-wioterminal.go
+++
@@ -1,8 +0,0 @@
-//go:build wioterminal
-
-package target
-
-func Pin(pin string) (GpioPin, bool) {
-	gpio, ok := AllTargets["wioterminal"].GpioPins[pin]
-	return gpio, ok
-}
