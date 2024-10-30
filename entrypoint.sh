a0955c95ac4bb7cf7006600cd2b16f94c4428989
Scott Feldman <sfeldma@gmail.com>
2024-10-29 18:44:00 -0700

    build arm/x86-64 archs before running hub to preload go packages/env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 echo "Warming up Go compiler cache..."
 
-# Compile a dummy Go program to preload Go packages
-echo 'package main; func main() {}' > /tmp/dummy.go
-go build -o /dev/null /tmp/dummy.go && rm /tmp/dummy.go
+# Compile the hub on different archs to preload Go packages.  This
+# makes subsequent compiles of devices by the hub itself much faster.
+
+echo "Building ARM arch..."
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w" -tags rpi -o /dev/null ./cmd
+echo "Building x86-64 arch..."
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -tags x86_64 -o /dev/null ./cmd
 
 echo "Go cache warmed up. Executing: $@"
 exec "$@"  # Run whatever command is passed from CMD (default is /hub)
