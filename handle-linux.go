1852adcf328ffd3787955f68ab5cc11ef91fc87e
Scott Feldman <sfeldma@gmail.com>
2024-10-31 09:31:52 -0700

    validate device handle path for leading '/'

--- handle-linux.go
+++ handle-linux.go
@@ -2,10 +2,16 @@
 
 package hub
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (d *device) handlersInstall() {
 	for path, handler := range d.Handlers {
+		if len(path) > 0 && path[0] != '/' {
+			LogError("Handler missing leading '/'", "path", path, "device", d)
+			continue
+		}
 		d.Handle("POST "+path, d.newPacketRoute(handler))
 	}
 }
