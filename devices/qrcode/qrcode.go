5c98baa1d618db843435f4157afddb3b08af86a3
Scott Feldman <sfeldma@gmail.com>
2024-11-03 01:12:23 -0800

    set default content when in demo mode

--- devices/qrcode/qrcode.go
+++ devices/qrcode/qrcode.go
@@ -8,6 +8,10 @@ func NewModel() hub.Devicer {
 	return &qrcode{}
 }
 
+func (q *qrcode) DemoSetup() error {
+	q.Content = "https://merliot.io"
+	return nil
+}
+
 func (q *qrcode) Poll(pkt *hub.Packet)     {}
-func (q *qrcode) DemoSetup() error         { return nil }
 func (q *qrcode) DemoPoll(pkt *hub.Packet) {}
