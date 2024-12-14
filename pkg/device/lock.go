a6a5f8b11686af68f33eb21df48c66176f72b51c
Scott Feldman <sfeldma@gmail.com>
2024-12-13 22:50:01 -0800

    switch to standard sync.Mutex

--- pkg/device/lock.go
+++ pkg/device/lock.go
@@ -2,11 +2,10 @@
 
 package device
 
-//import "sync"
+import "sync"
 
-import "github.com/ietxaniz/delock"
+//import "github.com/ietxaniz/delock"
 
-/*
 type mutex struct {
 	sync.Mutex
 }
@@ -14,8 +13,8 @@ type mutex struct {
 type rwMutex struct {
 	sync.RWMutex
 }
-*/
 
+/*
 type mutex struct {
 	mu delock.Mutex
 	id int
@@ -61,3 +60,4 @@ func (m *rwMutex) RLock() {
 func (m *rwMutex) RUnlock() {
 	m.mu.RUnlock(m.id)
 }
+*/
