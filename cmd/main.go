f2b746d82dc254795b01596737ee7487296ae438
Scott Feldman <sfeldma@gmail.com>
2025-04-03 21:32:07 -0700

    cleanup from go doc

--- cmd/main.go
+++ cmd/main.go
@@ -13,29 +13,38 @@
 package main
 
 import (
+	"os"
 	"strconv"
 
 	"github.com/merliot/hub/pkg/device"
 	"github.com/merliot/hub/pkg/models"
 )
 
+func getenv(name string, defaultValue string) string {
+	value, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultValue
+	}
+	return value
+}
+
 func main() {
 
-	port, _ := strconv.Atoi(device.Getenv("PORT", "8000"))
-	pingPeriod, _ := strconv.Atoi(device.Getenv("PING_PERIOD", "2"))
-	keepBuilds := device.Getenv("DEBUG_KEEP_BUILDS", "")
-	runningSite := device.Getenv("SITE", "")
-	runningDemo := device.Getenv("DEMO", "")
-	background := device.Getenv("BACKGROUND", "")
-	wifiSsids := device.Getenv("WIFI_SSIDS", "")
-	wifiPassphrases := device.Getenv("WIFI_PASSPHRASES", "")
-	autoSave := device.Getenv("AUTO_SAVE", "true")
-	devicesEnv := device.Getenv("DEVICES", "")
-	devicesFile := device.Getenv("DEVICES_FILE", "")
-	logLevel := device.Getenv("LOG_LEVEL", "INFO")
-	dialUrls := device.Getenv("DIAL_URLS", "")
-	user := device.Getenv("USER", "")
-	passwd := device.Getenv("PASSWD", "")
+	port, _ := strconv.Atoi(getenv("PORT", "8000"))
+	pingPeriod, _ := strconv.Atoi(getenv("PING_PERIOD", "2"))
+	keepBuilds := getenv("DEBUG_KEEP_BUILDS", "")
+	runningSite := getenv("SITE", "")
+	runningDemo := getenv("DEMO", "")
+	background := getenv("BACKGROUND", "")
+	wifiSsids := getenv("WIFI_SSIDS", "")
+	wifiPassphrases := getenv("WIFI_PASSPHRASES", "")
+	autoSave := getenv("AUTO_SAVE", "true")
+	devicesEnv := getenv("DEVICES", "")
+	devicesFile := getenv("DEVICES_FILE", "")
+	logLevel := getenv("LOG_LEVEL", "INFO")
+	dialUrls := getenv("DIAL_URLS", "")
+	user := getenv("USER", "")
+	passwd := getenv("PASSWD", "")
 
 	server := device.NewServer(
 		device.WithPort(port),
