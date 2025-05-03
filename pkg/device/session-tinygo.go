4fecce1904d0f7b41acb3af9214b931a923a3953
Scott Feldman <sfeldma@gmail.com>
2025-05-02 19:36:59 -0700

    don't fail session routeAll for tinygo; just do nothing

--- pkg/device/session-tinygo.go
+++ pkg/device/session-tinygo.go
@@ -2,10 +2,8 @@
 
 package device
 
-import "errors"
-
 type sessionMap struct{}
 
 func (sm *sessionMap) routeAll(pkt *Packet) (err error) {
-	return errors.New("Sessions.routeAll not implemented")
+	return nil
 }
