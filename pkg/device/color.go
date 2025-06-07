cc4fe2ebee4b6fcd535b7dfc5a0d4739c3b0bb2f
Scott Feldman <sfeldma@gmail.com>
2025-06-06 22:29:59 -0700

    Phase 2: Map custom color classes and template variables to Tailwind equivalents

--- pkg/device/color.go
+++ pkg/device/color.go
@@ -4,21 +4,44 @@ package device
 
 func (d *device) bgColor() string {
 	if d.Config.BgColor == "" {
-		return "bg-space-white"
+		return "bg-white"
 	}
-	return "bg-" + d.Config.BgColor
+	switch d.Config.BgColor {
+	case "sunflower":
+		return "bg-yellow-300"
+	case "gold":
+		return "bg-yellow-400"
+	case "text", "violet-creme":
+		return "bg-purple-200"
+	default:
+		return "bg-" + d.Config.BgColor
+	}
 }
 
 func (d *device) textColor() string {
 	if d.Config.FgColor == "" {
 		return "text-black"
 	}
-	return "text-" + d.Config.FgColor
+	switch d.Config.FgColor {
+	case "text", "violet-creme":
+		return "text-purple-200"
+	default:
+		return "text-" + d.Config.FgColor
+	}
 }
 
 func (d *device) borderColor() string {
 	if d.Config.BgColor == "" {
-		return "border-space-white"
+		return "border-white"
 	}
-	return "border-" + d.Config.BgColor
+	switch d.Config.BgColor {
+	case "sunflower":
+		return "border-yellow-300"
+	case "gold":
+		return "border-yellow-400"
+	case "text", "violet-creme":
+		return "border-purple-200"
+	default:
+		return "border-" + d.Config.BgColor
+	}
 }
